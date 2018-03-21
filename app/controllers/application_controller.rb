b8c1e1597d4115fb3e74406edafd5271324df533
Nitzan Ben Ner <nitzan.benner@lemonade.com>
2018-03-21 15:13:46 +0200

    Finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "¡Hola, mundo!"
-  end
-  def byebye
-    render html: "yes!"
-  end
 end
